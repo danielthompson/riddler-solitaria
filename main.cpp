ce73dde331a85dfad7f8ae1b6f50dd7e9c3b27fc
Daniel A. Thompson <me@danielthompson.net>
2018-08-20 15:17:45 -0500

    small bugfixes

--- main.cpp
+++ main.cpp
@@ -7,42 +7,45 @@
 
 int main() {
 
-   const std::string filePath = "/Users/daniel/Documents/solitaria2.txt";
-   std::ofstream logfile;
-   logfile.open(filePath, std::ios::trunc);
+   std::string header = "Population\tWinners\tLosers\tPercentage";
 
-   std::cout << "Population\tWinners\tPercentage" << std::endl;
-   logfile << "Population\tWinners\tPercentage" << std::endl;
+   std::cout << header << std::endl;
 
    // minimum size of population
    constexpr unsigned int minPopulation = 1000;
    constexpr unsigned int maxPopulation = 100000;
    constexpr unsigned int populationStepSize = 1000;
-   constexpr unsigned int iterationsPerStep = 100;
+   constexpr unsigned int iterationsPerStep = 1000;
 
    std::random_device randomDevice;
    std::mt19937 generator(randomDevice());
-   std::uniform_int_distribution<> distribution(0);
 
-   // placeholders per iteration
-   int winners = 0;
-   int losers = 0;
+   for (unsigned int populationSize = minPopulation; populationSize <= maxPopulation; populationSize += populationStepSize) {
 
-   for (int populationSize = minPopulation; populationSize <= maxPopulation; populationSize += populationStepSize) {
+      unsigned long winners = 0;
+      unsigned long losers = 0;
 
       std::vector<bool> people(populationSize, true);
-      for (int j = 0; j < iterationsPerStep; j++) {
+      for (unsigned int j = 0; j < iterationsPerStep; j++) {
 
-         int peopleRemaining = populationSize;
+         unsigned int peopleRemaining = populationSize;
 
          do {
-            for (int i = 0; i < peopleRemaining; i++) {
-               people[distribution(generator) % peopleRemaining] = false;
+            std::uniform_int_distribution<unsigned int> distribution(0, peopleRemaining - 1);
+
+            for (unsigned int i = 0; i < peopleRemaining; i++) {
+               unsigned int choice;
+               // subjects do not choose themselves, presumably
+               do {
+                  choice = distribution(generator);
+               }
+               while (choice == i);
+               people[choice] = false;
             }
 
-            int newPeopleRemaining = 0;
+            unsigned int newPeopleRemaining = 0;
 
-            for (int i = 0; i < peopleRemaining; i++) {
+            for (unsigned int i = 0; i < peopleRemaining; i++) {
                newPeopleRemaining = people[i] ? newPeopleRemaining + 1 : newPeopleRemaining;
                people[i] = true;
             }
@@ -54,27 +57,13 @@ int main() {
          winners = (peopleRemaining == 1) ? winners + 1 : winners;
          losers = (peopleRemaining == 0) ? losers + 1 : losers;
       }
-      float percentage = winners * populationStepSize / (float)(populationSize * iterationsPerStep);
+      float winningPercentage = winners / (float)(winners + losers);
+      float losingPercentage = losers / (float)(winners + losers);
 
       std::cout << std::fixed;
-      logfile << std::fixed;
 
-      std::cout << populationSize << "\t" << winners << "\t";
-      logfile << populationSize << "\t" << winners << "\t";
+      std::cout << populationSize << "\t\t" << winners << "\t\t" << losers << "\t\t";
 
-      std::cout << std::fixed;
-      logfile << std::fixed;
-
-      std::cout << std::setprecision(4);
-      logfile << std::setprecision(4);
-
-      std::cout << percentage ;
-      logfile << percentage ;
-
-      std::cout << std::endl;
-      logfile << std::endl;
-
-      logfile << std::flush;
+      std::cout << std::fixed << std::setprecision(4) << winningPercentage << std::endl;
    }
-   logfile.close();
 }
